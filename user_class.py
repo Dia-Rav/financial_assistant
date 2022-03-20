ad824d9cfef9eb94aed7203c6038bfa2dce14316
Dia-Rav <90519131+Dia-Rav@users.noreply.github.com>
2022-03-20 13:35:18 +0300

    Update user_class.py

--- user_class.py
+++ user_class.py
@@ -13,12 +13,6 @@ class user:
         return self.user_name
     def get_user_id(self):
         return self.user_id
-
-    #можно попробовать хранить информацию о покупках пользователя в его объекте
-    #def new_purchase (self, category, product_name, sum):
-    #    if category in self.purchases_current_month:
-    #        pass
-    #def next_month(self):
-    #    pass
+    
     #def get_previous_month (self):
     #    pass
