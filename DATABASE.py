4fa71670534092f27f2b10ec060443090a859520
AnfimovAleksandr <anfimov.ad@phystech.edu>
2022-04-06 21:33:19 +0300

    Функции по добавлению новых слов с категориями

--- DATABASE.py
+++ DATABASE.py
@@ -1,6 +1,48 @@
 import sqlite3
 
-def create_database():
+def create_database_A():
+    try:
+        sqlite_connection = sqlite3.connect('DATABASE.db')
+        sqlite_create_table_query = '''CREATE TABLE A_DATABASE (
+                                    word TEXT NOT NULL,
+                                    category TEXT NOT NULL,
+                                    frequency INTEGER NOT NULL);'''
+
+        cursor = sqlite_connection.cursor()
+        cursor.execute(sqlite_create_table_query)
+        sqlite_connection.commit()
+        print("Таблица статистики по категориям A_DATABASE создана")
+
+        cursor.close()
+
+    except sqlite3.Error as error:
+        print("Ошибка в блоке Creation_A: ", error)
+    finally:
+        if (sqlite_connection):
+            sqlite_connection.close()
+
+def create_database_B():
+    try:
+        sqlite_connection = sqlite3.connect('DATABASE.db')
+        sqlite_create_table_query = '''CREATE TABLE B_DATABASE (
+                                    id INTEGER NOT NULL,
+                                    category TEXT NOT NULL,
+                                    money_spent INTEGER NOT NULL);'''
+
+        cursor = sqlite_connection.cursor()
+        cursor.execute(sqlite_create_table_query)
+        sqlite_connection.commit()
+        print("Таблица финансов B_DATABASE создана")
+
+        cursor.close()
+
+    except sqlite3.Error as error:
+        print("Ошибка в блоке Creation_B: ", error)
+    finally:
+        if (sqlite_connection):
+            sqlite_connection.close()
+
+def create_database_C():
     try:
         sqlite_connection = sqlite3.connect('DATABASE.db')
         sqlite_create_table_query = '''CREATE TABLE C_DATABASE (
@@ -9,33 +51,41 @@ def create_database():
                                     category TEXT NOT NULL);'''
 
         cursor = sqlite_connection.cursor()
-        print("База данных подключена к SQLite")
         cursor.execute(sqlite_create_table_query)
         sqlite_connection.commit()
-        print("Таблица SQLite создана")
+        print("Таблица категорий пользователей C_DATABASE создана")
 
         cursor.close()
 
     except sqlite3.Error as error:
-        print("Ошибка в блоке Creation: ", error)
+        print("Ошибка в блоке Creation_C: ", error)
     finally:
         if (sqlite_connection):
             sqlite_connection.close()
-            print("Соединение с SQLite закрыто")
 
-def  insert_category(data_tuple):
+def insert_new_category(data_tuple):
     try:
         sqlite_connection = sqlite3.connect('DATABASE.db')
         cursor = sqlite_connection.cursor()
-        print("Подключен к SQLite")
 
-        sqlite_insert_with_param = """INSERT INTO C_DATABASE
+        sqlite_insert_A = """INSERT INTO A_DATABASE
+                              (word, category, frequency)
+                              VALUES (?, ?, ?);"""
+        sqlite_insert_B = """INSERT INTO B_DATABASE
+                              (id, category, money_spent)
+                              VALUES (?, ?, ?);"""
+        sqlite_insert_C = """INSERT INTO C_DATABASE
                               (id, word, category)
                               VALUES (?, ?, ?);"""
 
-        cursor.execute(sqlite_insert_with_param, data_tuple)
+        data_A = (data_tuple[2], data_tuple[1], 1)
+        data_B = (data_tuple[0], data_tuple[1], data_tuple[3])
+        data_C = (data_tuple[0], data_tuple[2], data_tuple[1])
+
+        cursor.execute(sqlite_insert_A, data_A)
+        cursor.execute(sqlite_insert_B, data_B)
+        cursor.execute(sqlite_insert_C, data_C)
         sqlite_connection.commit()
-        print("Переменные Python успешно вставлены в таблицу C_DATABASE")
 
         cursor.close()
 
@@ -44,21 +94,40 @@ def  insert_category(data_tuple):
     finally:
         if sqlite_connection:
             sqlite_connection.close()
-            print("Соединение с SQLite закрыто")
+
+def payment(data):
+    try:
+        sqlite_connection = sqlite3.connect('DATABASE.db')
+        cursor = sqlite_connection.cursor()
+
+        money_selection = """select money_spent from B_DATABASE where id = ? AND category = ?"""
+        cursor.execute(money_selection, (data[0], data[1],))
+        record = cursor.fetchall()
+        money = record[0][0]
+
+        sql_update_query = """Update B_DATABASE set money_spent = ? where id = ? and category = ?"""
+        money += data[2]
+        data = (money, data[0], data[1])
+        cursor.execute(sql_update_query, data)
+        sqlite_connection.commit()
+        cursor.close()
+
+    except sqlite3.Error as error:
+        print("Ошибка в блоке payment: ", error)
+    finally:
+        if sqlite_connection:
+            sqlite_connection.close()
 
 def get_dict(id):
     try:
         sqlite_connection = sqlite3.connect('DATABASE.db')
         cursor = sqlite_connection.cursor()
-        print("Подключен к SQLite")
 
         user_dict = dict()
         sql_select_query = """select word, category from C_DATABASE where id = ?"""
         cursor.execute(sql_select_query, (id,))
         records = cursor.fetchall()
-        print("Всего строк:  ", len(records))
         for row in records:
-            print(row)
             if row[1] not in user_dict.keys():
                 user_dict[row[1]] = set()
             user_dict[row[1]].add(row[0])
@@ -71,19 +140,16 @@ def get_dict(id):
     finally:
         if sqlite_connection:
             sqlite_connection.close()
-            print("Соединение с SQLite закрыто")
 
 def update_category(id, category_to_update, new_category):
     try:
         sqlite_connection = sqlite3.connect('DATABASE.db')
         cursor = sqlite_connection.cursor()
-        print("Подключен к SQLite")
 
         sql_update_query = """Update C_DATABASE set category = ? where id = ? and category = ?"""
         data = (new_category, id, category_to_update)
         cursor.execute(sql_update_query, data)
         sqlite_connection.commit()
-        print("Запись успешно обновлена")
         cursor.close()
 
     except sqlite3.Error as error:
@@ -91,18 +157,15 @@ def update_category(id, category_to_update, new_category):
     finally:
         if sqlite_connection:
             sqlite_connection.close()
-            print("Соединение с SQLite закрыто")
 
 def delete_user(id):
     try:
         sqlite_connection = sqlite3.connect('DATABASE.db')
         cursor = sqlite_connection.cursor()
-        print("Подключен к SQLite")
 
         sql_update_query = """DELETE from C_DATABASE where id = ?"""
         cursor.execute(sql_update_query, (id, ))
         sqlite_connection.commit()
-        print("Запись успешно удалена")
         cursor.close()
 
     except sqlite3.Error as error:
@@ -110,7 +173,32 @@ def delete_user(id):
     finally:
         if sqlite_connection:
             sqlite_connection.close()
-            print("Соединение с SQLite закрыто")
+
+def delete_ALL():
+    try:
+        sqlite_connection = sqlite3.connect('DATABASE.db')
+        cursor = sqlite_connection.cursor()
+
+        A = """DELETE FROM A_DATABASE"""
+        B = """DELETE FROM B_DATABASE"""
+        C = """DELETE FROM C_DATABASE"""
+        cursor.execute(A)
+        cursor.execute(B)
+        cursor.execute(C)
+        sqlite_connection.commit()
+        print("ВСЕ записи успешно удалены")
+        cursor.close()
+
+    except sqlite3.Error as error:
+        print("Ошибка в блоке delete_ALL: ", error)
+    finally:
+        if sqlite_connection:
+            sqlite_connection.close()
+
 
 if __name__ == "__main__":
-    delete_user(123)
+    #data = (123, 'фрукты', 'банан', 200)
+    #insert_new_category(data)
+
+    data = (123, 'фрукты', 500)
+    payment(data)
